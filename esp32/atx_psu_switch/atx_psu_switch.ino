4df712dda22084a08660ea570550b88aaed53f0e
Natasha Ament-Teusink <nwj.ament@gmail.com>
2024-01-24 20:01:50 +0100

    Update atx_psu_switch.ino

--- esp32/atx_psu_switch/atx_psu_switch.ino
+++ esp32/atx_psu_switch/atx_psu_switch.ino
@@ -1,1 +1,22 @@
-#include <wifi>
+#include <WiFi.h>
+
+const char* ssid = "<ssid>";
+const char* wpa2 = "<wpa2 passcode>";
+
+void setup()
+{
+  Serial.begin(115200)
+  Serial.println("Connecting to WiFi network";
+  WiFi.begin(ssid, wpa2);
+
+  if ((WiFi.status) == "WL_CONNECTED")
+{
+Serial.println("Connected");
+} 
+else {
+  Serial.println("Still waiting");
+}
+
+void loop()
+{
+}
